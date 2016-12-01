950e506db8a6f3d21bad08c9c8e34e5a872dfced
Peter Teuben <teuben@gmail.com>
2016-11-30 22:33:18 -0500

    checkin of the Haiti 2016 version

---
+++ notebooks/ch.py
@@ -0,0 +1,12 @@
+#
+#  example of modules
+#  CH.py
+#
+
+import math
+pi = math.e
+
+#  ch.py
+def hello(id="stranger"):
+    city = "Cap Hatien"
+    print("Welcome to %s, %s" % (city,id))
---
+++ notebooks/pap.py
@@ -0,0 +1,11 @@
+#
+#  example of modules
+#  PAP.py
+#
+
+
+# pap.py
+def hello(id="stranger"):
+    city = "Port au Prince"
+    print("Welcome to %s, %s" % (city,id))
+    
